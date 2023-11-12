0c60470a9d3c7cf947153b7d0b26393911ae5e89
Jasper <steenhuisjasper1@gmail.com>
2023-11-12 21:13:25 +0100

    basic adding tasks

--- studious-potato/src/App.tsx
+++ studious-potato/src/App.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import { useState } from "react";
 const ListInput = () =>{
   const [inputString, setInput] = useState("");
+  const [taskList, setTaskList] = useState([]);
   const handleChange = (e) =>{
     e.preventDefault();
     setInput(e.target.value);
   }
   const handleSubmit = (e) =>{
+    setTaskList([{title: inputString}, ...taskList ]);
     e.preventDefault();
-
+    setInput("");
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
         <input type="text" name="taskInput" placeholder="Enter taskname" onChange={handleChange}/>
-        <button>Add</button>
-      </form>
-      {inputString}
+        <button onClick={handleSubmit}>Add</button>
+        <ul>  
+        {taskList.map(task => 
+          <li>{task.title}</li>
+        )}
+        </ul>
     </div>
   );
 }
